d5f414bd5bf2d478525988d4e763080b6d08bc92
ashwani436 <sharmagpl786@gmail.com>
2021-02-23 00:16:34 +0530

    save changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
-import React from "react";
-import "./../styles/App.css";
+import React from 'react';
+import "../styles/App.css";
+import TodoList from '../components/TodoList';
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
 }
-
 
 export default App;
